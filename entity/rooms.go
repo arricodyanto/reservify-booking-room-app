4c4cee809e2df51567b91c92a7d6ac353dbcf05e
arricodyanto <arricohandyanto@gmail.com>
2023-12-19 14:57:38 +0700

    :heavy_plus_sign: add: room repository & query config

--- entity/rooms.go
+++ entity/rooms.go
@@ -2,7 +2,7 @@ package entity
 
 import "time"
 
-type Rooms struct {
+type Room struct {
 	ID        string    `json:"id"`
 	Name      string    `json:"name"`
 	RoomType  string    `json:"room_type"`
--- repository/rooms_repository.go
+++ repository/rooms_repository.go
@@ -1,17 +1,18 @@
 package repository
 
 import (
+	"booking-room-app/config"
 	"booking-room-app/entity"
-	"booking-room-app/entity/dto"
 	"database/sql"
+	"log"
 )
 
 type RoomRepository interface {
-	List() ([]entity.Rooms, error)
-	Get(id string) (entity.Rooms, error)
-	Create(payload entity.Rooms) (entity.Rooms, error)
-	Update(payload entity.Rooms) (entity.Rooms, error)
-	UpdateStatus(payload dto.RoomsDto) (dto.RoomsDto, error)
+	Create(payload entity.Room) (entity.Room, error)
+	Get(id string) (entity.Room, error)
+	List() ([]entity.Room, error)
+	Update(payload entity.Room) (entity.Room, error)
+	UpdateStatus(payload entity.Room) (entity.Room, error)
 }
 
 type roomRepository struct {
@@ -19,28 +20,93 @@ type roomRepository struct {
 }
 
 // Create implements RoomRepository.
-func (*roomRepository) Create(payload entity.Rooms) (entity.Rooms, error) {
-	panic("unimplemented")
+func (r *roomRepository) Create(payload entity.Room) (entity.Room, error) {
+	var room entity.Room
+	err := r.db.QueryRow(config.InsertRoom, payload.Name, payload.RoomType, payload.Capacity, payload.Status, payload.UpdatedAt).Scan(&room.ID, &room.CreatedAt)
+	if err != nil {
+		log.Println("roomRepository.CreateQueryRow", err.Error())
+		return entity.Room{}, err
+	}
+
+	room.Name = payload.Name
+	room.RoomType = payload.RoomType
+	room.Capacity = payload.Capacity
+	room.Status = payload.Status
+	room.UpdatedAt = payload.UpdatedAt
+
+	return room, nil
 }
 
 // Get implements RoomRepository.
-func (*roomRepository) Get(id string) (entity.Rooms, error) {
-	panic("unimplemented")
+func (r *roomRepository) Get(id string) (entity.Room, error) {
+	var room entity.Room
+	err := r.db.QueryRow(config.SelectRoomByID, id).Scan(&room.ID, &room.Name, &room.RoomType, &room.Capacity, &room.Status, &room.CreatedAt, &room.UpdatedAt)
+	if err != nil {
+		log.Println("roomRepository.GetQueryRow", err.Error())
+		return entity.Room{}, err
+	}
+
+	return room, nil
 }
 
 // List implements RoomRepository.
-func (*roomRepository) List() ([]entity.Rooms, error) {
-	panic("unimplemented")
+func (r *roomRepository) List() ([]entity.Room, error) {
+	var rooms []entity.Room
+	rows, err := r.db.Query(config.SelectRoomList)
+	if err != nil {
+		log.Println("roomRepository.ListQuery", err.Error())
+		return []entity.Room{}, err
+	}
+
+	for rows.Next() {
+		var room entity.Room
+		err := rows.Scan(&room.ID, &room.Name, &room.RoomType, &room.Capacity, &room.Status, &room.CreatedAt, &room.UpdatedAt)
+		if err != nil {
+			log.Println("roomRepository.ListScan", err.Error())
+			return []entity.Room{}, err
+		}
+
+		rooms = append(rooms, room)
+	}
+
+	return rooms, nil
 }
 
 // Update implements RoomRepository.
-func (*roomRepository) Update(payload entity.Rooms) (entity.Rooms, error) {
-	panic("unimplemented")
+func (r *roomRepository) Update(payload entity.Room) (entity.Room, error) {
+	var room entity.Room
+	room.ID = payload.ID
+
+	err := r.db.QueryRow(config.UpdateRoomByID, room.ID, payload.Name, payload.RoomType, payload.Capacity, payload.Capacity, payload.Status, payload.UpdatedAt).Scan(&room.CreatedAt)
+	if err != nil {
+		log.Println("roomRepository.UpdateQueryRow", err.Error())
+		return entity.Room{}, err
+	}
+
+	room.Name = payload.Name
+	room.RoomType = payload.RoomType
+	room.Capacity = payload.Capacity
+	room.Status = payload.Status
+	room.UpdatedAt = payload.UpdatedAt
+
+	return room, nil
 }
 
 // UpdateStatus implements RoomRepository.
-func (*roomRepository) UpdateStatus(payload dto.RoomsDto) (dto.RoomsDto, error) {
-	panic("unimplemented")
+func (r *roomRepository) UpdateStatus(payload entity.Room) (entity.Room, error) {
+	var room entity.Room
+	room.ID = payload.ID
+
+	err := r.db.QueryRow(config.UpdateRoomStatus, room.ID, payload.Status, payload.UpdatedAt).Scan(&room.Name, &room.RoomType, &room.Capacity, &room.CreatedAt)
+	if err != nil {
+		log.Println("roomRepository.UpdateStatusQueryRow", err.Error())
+		return entity.Room{}, err
+	}
+
+	room.Status = payload.Status
+	room.UpdatedAt = payload.UpdatedAt
+
+	return room, nil
 }
 
 // create room (ADMIN) -GET
