7848dbaa294cb2c238d00c7cb80f02a5519c33d6
taufiq xy <taufiq.xy@gmail.com>
2023-12-19 22:52:49 +0700

    :white_check_mark: update: raw SQL query

--- config/raw_query.go
+++ config/raw_query.go
@@ -1,9 +1,9 @@
 package config
 
 const (
-	SelectRoomFacilityList = `SELECT id, room_id, facility_id, quantity, created_at, updated_at FROM trx_room_facility`
+	SelectRoomFacilityList = `SELECT id, room_id, facility_id, quantity, created_at, updated_at FROM trx_room_facility ORDER BY created_at DESC LIMIT $1 OFFSET $2`
 	SelectRoomFacilityByID = ``
 	InsertRoomFacility     = ``
 	UpdateRoomFacility     = ``
-	GetIdListRoomFacility  = ``
+	GetCountRoomFacility   = `SELECT COUNT(*) FROM trx_room_facility`
 )
