fb9fcb44677a82e2d29e49a73e968ee5ad602bf5
Fachri Ibnu Falah <fahriibnu1234@gmail.com>
2023-12-19 10:16:20 +0700

    add: todo list create fiture

--- repository/employees_repository.go
+++ repository/employees_repository.go
@@ -1,1 +1,10 @@
 package repository
+
+// get all employees (GA, ADMIN) -GET
+// get employees by id (GA, ADMIN) -GET
+// create employee (ADMIN) -POST
+// update employee (ADMIN) -POST
+// delete an employee (ADMIN) -DELETE
+
+
+
--- repository/transactions_repository.go
+++ repository/transactions_repository.go
@@ -1,1 +1,9 @@
 package repository
+
+//
+// list transaction (admin & GA) -GET
+// get transaction by id (GA) - GET /transactions/:id
+// list transaction by employee ID (employee) -GET
+// Request booking rooms (employee & admin) -POST
+// permission list (GA) -GET
+// update permission (GA) -PUT
