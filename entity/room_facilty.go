4a5ecca213312c641c8adfb0458a237d02b0097f
taufiq xy <taufiq.xy@gmail.com>
2023-12-19 14:51:00 +0700

    :white_check_mark: update: entity transaction room_facility

--- entity/room_facilty.go
+++ entity/room_facilty.go
@@ -1,4 +1,12 @@
 package entity
 
+import "time"
+
 type RoomFacility struct {
+	Id         string    `json:"id"`
+	RoomId     string    `json:"roomId"`
+	FacilityId string    `json:"facilityId"`
+	Quantity   int       `json:"quantity"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
 }
