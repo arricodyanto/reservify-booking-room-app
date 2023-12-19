32d96e98fe3649fa9f4ba3cdfa8ea0ca3cd7cb6e
taufiq xy <taufiq.xy@gmail.com>
2023-12-19 21:51:57 +0700

    :white_check_mark: update: raw SQL query

--- config/raw_query.go
+++ config/raw_query.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	SelectRoomFacilityList = ``
+	SelectRoomFacilityList = `SELECT id, room_id, facility_id, quantity, created_at, updated_at FROM trx_room_facility`
 	SelectRoomFacilityByID = ``
 	InsertRoomFacility     = ``
 	UpdateRoomFacility     = ``
