75fcc0d9ad8401c2bb88fc2dea8deace81d68c8d
RizkiRamadhandwi <rizki.ramadhandwi31@gmail.com>
2023-12-19 21:26:19 +0700

    add: config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type DbConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	Driver   string
+}
+
+type ApiConfig struct {
+	ApiPort string
+}
+
+type Config struct {
+	DbConfig
+	ApiConfig
+}
+
+func (c *Config) ConfigConfiguration() error {
+	err := godotenv.Load()
+	if err != nil {
+		return fmt.Errorf("missing env file %v", err.Error())
+	}
+	c.DbConfig = DbConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Name:     os.Getenv("DB_NAME"),
+		Driver:   os.Getenv("DB_DRIVER"),
+	}
+
+	c.ApiConfig = ApiConfig{ApiPort: os.Getenv("API_PORT")}
+
+	return nil
+}
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := cfg.ConfigConfiguration(); err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
