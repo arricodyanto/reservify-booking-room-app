6290516a16a7dcffa5a7482ac7292c6d55b78be0
RizkiRamadhandwi <rizki.ramadhandwi31@gmail.com>
2023-12-19 21:24:47 +0700

    add: shared

--- shared/common/json_response.go
+++ shared/common/json_response.go
@@ -1,1 +1,36 @@
 package common
+
+import (
+	"booking-room-app/shared/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SendCreateResponse(ctx *gin.Context, data interface{}, message string) {
+	ctx.JSON(http.StatusCreated, &model.SingleResponse{
+
+		Status: model.Status{
+			Code:    http.StatusCreated,
+			Message: message,
+		},
+		Data: data,
+	})
+}
+
+func SendSingleResponse(ctx *gin.Context, data interface{}, message string) {
+	ctx.JSON(http.StatusOK, &model.SingleResponse{
+		Status: model.Status{
+			Code:    http.StatusOK,
+			Message: message,
+		},
+		Data: data,
+	})
+}
+
+func SendErrorResponse(ctx *gin.Context, code int, message string) {
+	ctx.AbortWithStatusJSON(code, &model.Status{
+		Code:    code,
+		Message: message,
+	})
+}
