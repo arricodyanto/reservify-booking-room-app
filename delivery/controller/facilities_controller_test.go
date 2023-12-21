30cfd48df57db1567f3ef129e7094f20a3f4db13
RizkiRamadhandwi <rizki.ramadhandwi31@gmail.com>
2023-12-21 16:06:37 +0700

    add: unit testing

--- delivery/controller/facilities_controller_test.go
+++ delivery/controller/facilities_controller_test.go
@@ -4,8 +4,10 @@ import (
 	"booking-room-app/entity"
 	usecase_mock "booking-room-app/mock/usecase"
 	"booking-room-app/shared/model"
+	"errors"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
@@ -35,6 +37,7 @@ var expectedFasilities = entity.Facilities{
 	UpdatedAt: time.Now(),
 }
 
+// test list
 func (suite *FacilitiesControllerTestSuite) TestListHandler_Success() {
 	mockFacility := []entity.Facilities{expectedFasilities}
 	mockPaging := model.Paging{
@@ -43,16 +46,206 @@ func (suite *FacilitiesControllerTestSuite) TestListHandler_Success() {
 		TotalRows:   5,
 		TotalPages:  1,
 	}
-	suite.fum.On("FindAllFacilities").Return(mockFacility, mockPaging, nil)
+	suite.fum.On("FindAllFacilities", 1, 5).Return(mockFacility, mockPaging, nil)
 	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
-	request, err := http.NewRequest(http.MethodGet, "/api/v1/facilities", nil)
+	request, err := http.NewRequest(http.MethodGet, "/api/v1/facilities?page=1&size=5", nil)
 	assert.NoError(suite.T(), err)
+
 	responseRecorder := httptest.NewRecorder()
 	ctx, _ := gin.CreateTestContext(responseRecorder)
 	ctx.Request = request
 	ctx.Set("facilities", mockFacility)
 	handlerFunc.listHandler(ctx)
+
 	assert.Equal(suite.T(), http.StatusOK, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestListHandler_Fail() {
+	mockFacility := []entity.Facilities{expectedFasilities}
+	mockError := errors.New("something went wrong")
+
+	suite.fum.On("FindAllFacilities", 1, 5).Return(mockFacility, model.Paging{}, mockError)
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	request, err := http.NewRequest(http.MethodGet, "/api/v1/facilities?page=1&size=5", nil)
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.listHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusInternalServerError, responseRecorder.Code)
+}
+
+// test gethandler
+func (suite *FacilitiesControllerTestSuite) TestGetHandler_Success() {
+	// mockID := "1"
+	suite.fum.On("FindFacilitiesById", "").Return(expectedFasilities, nil)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	request, err := http.NewRequest(http.MethodGet, "/api/v1/facilities/1", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.getHandler(ctx)
+
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), http.StatusOK, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestGetHandler_Error() {
+
+	mockError := errors.New("facility not found")
+	suite.fum.On("FindFacilitiesById", "").Return(expectedFasilities, mockError)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	request, err := http.NewRequest(http.MethodGet, "/api/v1/facilities/1", nil)
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.getHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusNotFound, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestCreateHandler_Success() {
+	// Simulate a successful scenario
+	mockPayload := entity.Facilities{
+		Name:     "This is name",
+		Quantity: 10,
+	}
+	mockFacility := expectedFasilities
+
+	suite.fum.On("RegisterNewFacilities", mockPayload).Return(mockFacility, nil)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	requestBody := `{"name": "This is name", "quantity": 10}`
+	request, err := http.NewRequest(http.MethodPost, "/api/v1/facilities", strings.NewReader(requestBody))
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.createHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusCreated, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestCreateHandler_BadRequest() {
+	// Simulate a scenario where binding the JSON payload fails
+	mockPayload := entity.Facilities{}
+	mockError := errors.New("example error message")
+
+	// Mock the ShouldBindJSON method to return an error
+	suite.fum.On("RegisterNewFacilities", &mockPayload).Return(expectedFasilities, mockError)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	request, err := http.NewRequest(http.MethodPost, "/api/v1/facilities", nil)
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.createHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestCreateHandler_InternalServerError() {
+	mockPayload := entity.Facilities{
+		Name:     "This is name",
+		Quantity: 10,
+	}
+	mockError := errors.New("example error message")
+
+	suite.fum.On("RegisterNewFacilities", mockPayload).Return(expectedFasilities, mockError)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	requestBody := `{"name": "This is name", "quantity": 10}`
+	request, err := http.NewRequest(http.MethodPost, "/api/v1/facilities", strings.NewReader(requestBody))
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.createHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusInternalServerError, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestUpdateHandler_Success() {
+	// Simulate a successful scenario
+	mockPayload := entity.Facilities{
+		ID:       "1",
+		Name:     "This is name",
+		Quantity: 10,
+	}
+	mockFacility := expectedFasilities
+
+	suite.fum.On("EditFacilities", mockPayload).Return(mockFacility, nil)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	requestBody := `{"id": "1","name": "This is name", "quantity": 10}`
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/facilities", strings.NewReader(requestBody))
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.updateHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusCreated, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestUpdateHandler_BadRequest() {
+	mockPayload := entity.Facilities{}
+	mockError := errors.New("example error message")
+
+	suite.fum.On("EditFacilities", &mockPayload).Return(expectedFasilities, mockError)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/facilities", nil)
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.updateHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, responseRecorder.Code)
+}
+
+func (suite *FacilitiesControllerTestSuite) TestUpdateHandler_NotFound() {
+	mockPayload := entity.Facilities{
+		ID: "nonexistent_id",
+	}
+	mockError := errors.New("not found ID " + mockPayload.ID)
+
+	suite.fum.On("EditFacilities", mockPayload).Return(expectedFasilities, mockError)
+
+	handlerFunc := NewFacilitiesController(suite.fum, suite.rg)
+	requestBody := `{"id": "nonexistent_id"}`
+	request, err := http.NewRequest(http.MethodPut, "/api/v1/facilities", strings.NewReader(requestBody))
+	assert.NoError(suite.T(), err)
+
+	responseRecorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(responseRecorder)
+	ctx.Request = request
+
+	handlerFunc.updateHandler(ctx)
+
+	assert.Equal(suite.T(), http.StatusNotFound, responseRecorder.Code)
 }
 
 func TestFacilitiesControllerTestSuite(t *testing.T) {
