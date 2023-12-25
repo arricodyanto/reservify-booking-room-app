2eabd8f8b5eb9e5d5711c8c0e413c0f5c822e218
taufiq xy <taufiq.xy@gmail.com>
2023-12-25 12:42:41 +0700

    :heavy_minus_sign: remove: unused code

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"booking-room-app/delivery"
-	"fmt"
 )
 
 func main() {
 	delivery.NewServer().Run()
-}
-
-func CalculateArea(panjang int, lebar int) string {
-	if panjang > 0 && lebar > 0 {
-		if panjang > 100 && lebar > 100 {
-			area := panjang * lebar
-			return fmt.Sprintf("luas persegi panjang huge: %d", area)
-		}
-		area := panjang * lebar
-		return fmt.Sprintf("luas persegi panjang normal: %d", area)
-	}
-
-	return "invalid panjang or lebar"
 }
